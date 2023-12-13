beaa3fe2a0f3af18daf2060309fbbdc841e5cf24
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-13 17:01:14 +0530

    Create list_index.py

---
+++ list_index.py
@@ -0,0 +1,9 @@
+list1 = ['mercury','venus', 'earth', 'mars', 'jupiter', 'saturn']
+
+print("My list:", list1)
+
+print(list1[2])
+
+#Using list() constructor
+list2 = list(("ajit", "garg", "lucknow", 8))
+print(list2)
