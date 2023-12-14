5da9ee3a75df25fb593de2b64bca177e9d8c6602
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-14 11:30:59 +0530

    Create accessing_list_items.py

---
+++ accessing_list_items.py
@@ -0,0 +1,10 @@
+list2 = ['mercury','venus', 'earth', 'mars', 'jupiter', 'saturn', 'neptune', 'uranus']
+
+print(list2[:3])
+print(list2[3])
+print(list2[-4:])
+
+if "octava" in list2:
+    print("Mars is the fourth planet of solar system.")
+else: 
+    print("This is not our Solar System")
