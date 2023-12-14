28c7cb568b4ab1a15c81ab6fb14bab657e7ffde4
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-14 20:13:03 +0530

    Update remove_list_items.py

--- remove_list_items.py
+++ remove_list_items.py
@@ -1,5 +1,8 @@
 list1 = ['a', 'b', 'b', 'c', 'd', 'e']
 print(list1)
 print("Let's remove the duplicate b")
+
+#If there is more than one item with the specified value, the remove() method removes the first occurrence.
+
 list1.remove('b')
 print(list1)
