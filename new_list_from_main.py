6edbabfa625de5215b42788929f0edf26adeb16b
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-16 17:54:03 +0530

    Update new_list_from_main.py

--- new_list_from_main.py
+++ new_list_from_main.py
@@ -1,15 +1,15 @@
-main_list = ['pen', 'notebook', 'clothes', 'watch', 'cake', 'camera', 'football']
+main_list = ['apple pie', 'banana chips', 'orange juice', 'banana shake', 'apple custard', 'vanilla-apple cake', 'banana peanut butter sandwich']
 print(main_list)
 new_list1 = []       #declaring the list
 print("")
 print("Without List Comprehension we use for loop")
 for x in main_list:
-    if 'c' in x:
+    if 'apple' in x:
         new_list1.append(x)
 
 print(new_list1)
 print("")
 print("With List Comprehension we can do this in one line")
-new_list2 = [x for x in main_list if 'c' in x]
+new_list2 = [x for x in main_list if 'apple' in x]
 print("new_list = [x for x in main_list if 'c' in x]")
 print(new_list2)
