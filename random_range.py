83be2a8416123730382fda2b0cf2a0b360783eb5
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-19 23:38:11 +0530

    Create random_range.py

---
+++ random_range.py
@@ -0,0 +1,10 @@
+import random
+print(random.randrange(20, 50))
+
+
+#List Comprehension and random range
+new_list = [random.randrange(15,35) for x in range(10) if x % 2 == 0]
+print(new_list)
+
+new_list2 = [x for x in new_list if x % 2 == 0]
+print(new_list2)
